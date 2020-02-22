f47d2f725febf402f9c0d3e7b2e2875c5d134108
gh05t-pl <gozetero@gmail.com>
2020-02-22 13:06:52 +0100

    added mail service

--- app/src/Utils/Mails/MailService.php
+++ app/src/Utils/Mails/MailService.php
@@ -3,8 +3,36 @@
 
 namespace App\Utils\Mails;
 
+use App\Model\MailSendRequest;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
 
 class MailService
 {
+	private $transport;
+
+	public function __construct(
+		TransportInterface $transport
+	)
+	{
+		$this->transport = $transport;
+	}
+
+	public function setTransport(TransportInterface $transport)
+	{
+		$this->transport = $transport;
+	}
+
+	public function send(MailSendRequest $request)
+	{
+		$this->transport->send(
+			(new Email())
+				->from($request->getFrom())
+				->to($request->getTo())
+				->subject($request->getTitle())
+				->text($request->getBody())
+		);
+	}
+
 
 }
